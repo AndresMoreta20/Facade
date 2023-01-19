d421bf1228ce49953cc536f6456a442ca662e358
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-01-19 09:33:34 -0500

    se aumenta menu

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -5,15 +5,48 @@ class Program
     static void Main(string[] args)
     {
         FachadaForma dibujaFiguras= new FachadaForma();
-        dibujaFiguras.dibujarTriangulo();
+        
+     
+
+
+       
+
+
+
         
 
+        bool continuar = true;
 
-        dibujaFiguras.dibujarCirculo();
+        while (continuar)
+{
+            Console.WriteLine("Seleccione una figura");
+            Console.WriteLine("1. Rectangulo");
+            Console.WriteLine("2. Circulo");
+            Console.WriteLine("3. Triangulo");
+            Console.WriteLine("4. Salir");
 
+            string menu = Console.ReadLine();
 
+            switch (menu)
+            {
+                case "1":
+                    dibujaFiguras.dibujarRectangulo();
+                    break;
 
-        dibujaFiguras.dibujarRectangulo();
+                case "2":
+                    dibujaFiguras.dibujarCirculo();
+                    break;
+
+                case "3":
+                    dibujaFiguras.dibujarTriangulo();
+                    break;
+
+                case "4":
+                default:
+                    continuar = false;
+                    break;
+            }
+        }
     }
 
 }
